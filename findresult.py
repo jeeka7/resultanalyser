cba387a6e942add023e7cb27a2b3fee53e636a12
Dean Hunter <jeeka7@gmail.com>
2022-10-26 02:00:14 +0530

    Create findresult.py

---
+++ findresult.py
@@ -0,0 +1,17 @@
+import sys
+import pandas as pd
+import streamlit as st
+batch_size=int(input("how many students are there in your class?"))
+if batch_size >10:
+    sys.exit("Batch size cannot be greater than 10")
+st.write(batch_size)
+D={}
+# Add student information
+# to the dictionary
+for i in range(1,batch_size+1):
+    roll = i
+    marks = int(input('Enter Marks: '))
+    D[roll] = marks
+st.write(D)
+df=pd.DataFrame(D)
+st.write(df)
