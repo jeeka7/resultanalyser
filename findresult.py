4e5dfac246d068459d4e4ecec861169bfb4a2664
Dean Hunter <jeeka7@gmail.com>
2022-10-27 22:37:24 +0530

    Update findresult.py

--- findresult.py
+++ findresult.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
 st.title("My app is deployed on cloud and is working")
+
+import sys
+import pandas as pd
+batch_size=int(input("how many students are there in your class?"))
+if batch_size >10:
+    sys.exit("Batch size cannot be greater than 10")
+st.write(batch_size)
+D={}
+# Add student information
+# to the dictionary
+for i in range(1,batch_size+1):
+    roll = i
+    marks = int(input('Enter Marks: '))
+    D[roll] = marks
+st.write(D)
+df=pd.DataFrame(D)
+st.write(df)
