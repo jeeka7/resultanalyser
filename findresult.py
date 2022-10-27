36b9565c635e3a0b8e9ac274aa9aea8f0e9f06eb
Dean Hunter <jeeka7@gmail.com>
2022-10-28 04:32:52 +0530

    Update findresult.py

--- findresult.py
+++ findresult.py
@@ -4,10 +4,12 @@ import pandas as pd
 marksList =[]
 
 batch_size = st.slider("How many students are ther in your class?",1,10)
+total = st.slider("What is the maximum marks for this paper ?",5,100,100,0.5)
 
-for i in range(batch_size):
-  number = st.number_input('Insert a number')
-  marksList.append(number)
+for i in range(batch_size+1):
+  st.write(i+1)
+  marks = st.slider("What is the marks obtained ?",5,100,10,0.5)
+  marksList.append(marks)
   st.write(i)
-st.write('The current number is ', number)
+
 st.write(type(marksList))
