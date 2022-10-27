9f03763e727aad7a44405a16b67eeaaa84c024ea
Dean Hunter <jeeka7@gmail.com>
2022-10-27 22:56:07 +0530

    Update findresult.py

--- findresult.py
+++ findresult.py
@@ -3,3 +3,13 @@ import streamlit as st
 batch_size = st.slider('How many students are there?', 0, 10)
 
 st.write(batch_size)
+
+D={}
+
+for i in range(1,batch_size+1):
+    roll = i
+    marks = st.number_input('Insert marks')
+    D[roll] = marks
+print(D)
+df=pd.DataFrame(D)
+print(df)
