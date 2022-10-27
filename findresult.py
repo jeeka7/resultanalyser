36f22b7b048c192702c8984d9906b35e27e45d20
Dean Hunter <jeeka7@gmail.com>
2022-10-28 04:12:42 +0530

    Update findresult.py

--- findresult.py
+++ findresult.py
@@ -6,6 +6,7 @@ marksList =[]
 batch_size = st.slider("How many students are ther in your class?",1,10)
 
 for i in range(batch_size):
+  st.write("enter marks of",i)
   marksList.append(int(input("Enter marks")))
 
 st.write(type(marksList))
