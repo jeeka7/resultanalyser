7278108c7d7420defe650918a85f13ce3e29940e
Dean Hunter <jeeka7@gmail.com>
2022-10-28 04:35:21 +0530

    Update findresult.py

--- findresult.py
+++ findresult.py
@@ -6,10 +6,8 @@ marksList =[]
 batch_size = st.slider("How many students are ther in your class?",1,10)
 total = st.slider("What is the maximum marks for this paper ?",5,100,100,0.5)
 
-for i in range(batch_size+1):
-  st.write(i+1)
-  marks = st.slider("What is the marks obtained ?",5,100,10,0.5)
-  marksList.append(marks)
-  st.write(i)
+for i in range(batch_size):
+  marksList[i] = st.slider("What is the marks obtained ?",5,100,10,0.5)
 
 st.write(type(marksList))
+st.write(marksList)
