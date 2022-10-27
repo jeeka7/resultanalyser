2c79cb65a241751bff84c4bd5acf71393969741b
Dean Hunter <jeeka7@gmail.com>
2022-10-28 04:24:26 +0530

    Update findresult.py

--- findresult.py
+++ findresult.py
@@ -7,7 +7,7 @@ batch_size = st.slider("How many students are ther in your class?",1,10)
 
 for i in range(batch_size):
   number = st.number_input('Insert a number')
-  marksList.append(int(input("Enter marks")))
+  marksList.append(number)
   st.write(i)
 st.write('The current number is ', number)
 st.write(type(marksList))
