64c7453650c282f8f09c2e3723fc76a29460f5ad
Dean Hunter <jeeka7@gmail.com>
2022-10-30 07:22:06 +0530

    Update findresult.py

--- findresult.py
+++ findresult.py
@@ -9,9 +9,9 @@ df
 st.sidebar.header("Enter marks here")
 marks_form = st.sidebar.form("Enter each student marks orderwise")
 
-roll_number = marks_form.number_input("Roll")
-marks_obtained = marks_form.number_input("Marks")
-add_data = options_form.form_submit_button()
+roll_number = marks_form.number_input("Roll",1,100,1,1)
+marks_obtained = marks_form.number_input("Marks",0,100,0,0.5)
+add_data = marks_form.form_submit_button()
 if add_data:
   st.write(roll_number,marks)
   new_data = {"roll": roll_number, "marks":marks_obtained}
