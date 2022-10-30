1c017781e7c326e00632d3d11c94074fba134d4c
Dean Hunter <jeeka7@gmail.com>
2022-10-30 07:26:15 +0530

    Update findresult.py

--- findresult.py
+++ findresult.py
@@ -12,7 +12,6 @@ roll_number = marks_form.number_input("Roll",1,100,1,1)
 marks_obtained = marks_form.number_input("Marks",0.00,100.00,0.00,0.50)
 add_data = marks_form.form_submit_button()
 if add_data:
-  st.write(roll_number,marks)
   new_data = {"roll": roll_number, "marks":marks_obtained}
   df = df.append(new_data, ignore_index = True)
   df.to_csv("report.csv", index=False)
