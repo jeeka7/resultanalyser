2fa7496b78bd6d7e0a214d916b163b190dde2dc7
Dean Hunter <jeeka7@gmail.com>
2022-10-30 07:29:39 +0530

    Update findresult.py

--- findresult.py
+++ findresult.py
@@ -8,10 +8,10 @@ df
 
 st.sidebar.header("Enter marks here")
 marks_form = st.sidebar.form("Enter each student marks orderwise")
-roll_number = marks_form.number_input("Roll",1,100,1,1)
+roll = marks_form.number_input("roll",1,100,1,1)
 marks_obtained = marks_form.number_input("Marks",0.00,100.00,0.00,0.50)
 add_data = marks_form.form_submit_button()
 if add_data:
-  new_data = {"roll": roll_number, "marks":marks_obtained}
+  new_data = {"roll": roll, "marks":marks_obtained}
   df = df.append(new_data, ignore_index = True)
   df.to_csv("report.csv", index=False)
